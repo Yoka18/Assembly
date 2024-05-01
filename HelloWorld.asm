4c9abe4665ae25093e4bcb3129364bf6f3c10e48
inmisin <omeroflu2@gmail.com>
2024-05-01 23:24:43 +0300

    Update HelloWorld.asm

--- HelloWorld.asm
+++ HelloWorld.asm
@@ -4,12 +4,12 @@ section .data
  global _start
  
  _start:
-  mov eax, 4 ; system call 4 yazdır
+  mov eax, 4 ; system call
   mov ebx, 1 ; stdout dosya tanımlayıcısı 
   mov ecx, msg
   mov edx, 13 ; mesajın uzunluğu kadar
   
   int 0x80 ; sistem çağrısı yap
-  mov eax, 1 ; sistemi 1 ile sonlandır
+  mov eax, 1 ; sistemi sonlandır
   xor ebx, ebx ;
   int 0x80
